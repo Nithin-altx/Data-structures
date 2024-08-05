5ab1d9611726a4f14fe6ed5be3c0dbfe543295f1
Nithin Emmadishetti <nithin200133@gmail.com>
2024-08-05 17:46:40 +0530

    added kth_distinct_string_in_an_array.cpp

---
+++ kth_distinct_string_in_an_array.cpp
@@ -0,0 +1,21 @@
+class Solution {
+Input: arr = ["d","b","c","b","c","a"], k = 2
+Output: "a"
+public:
+    string kthDistinct(vector<string>& arr, int k) {
+     map<string,int>mp;
+     for(auto &v:arr)
+     {
+        mp[v]++;//mapping the string to unique d->1,b->2,c->2,a->1
+     }
+     for(auto &it:arr)
+     {
+        if(mp[it]==1)//iterating through the array and finding tha string kth unique string
+        k--;
+        if(k==0)
+        return it;returning the kth unique string
+     }
+     return "";
+        
+    }
+};
