e015d7828953e5649a83776fd792fc241b8bd7a6
Nithin Emmadishetti <nithin200133@gmail.com>
2024-09-02 12:06:41 +0530

    modified Palindrome_partiton_bottomup_2.java Tc=O(n^2)

--- Palindrome_partiton_bottomup_2.java
+++ Palindrome_partiton_bottomup_2.java
@@ -1,9 +1,11 @@
 class Solution {
-public:
-int solve(string &s)
-{
-    int n=s.length();
-        vector<vector<bool>>t(n,vector<bool>(n,false));
+    public int minCut(String s) {
+        int n=s.length();
+        boolean t[][]=new boolean[2001][2001];
+        for(int i=0;i<2001;i++)
+        {
+            Arrays.fill(t[i],false);
+        }
         for(int i=0;i<n;i++)
         {
             t[i][i]=true;
@@ -12,27 +14,27 @@ int solve(string &s)
         {
             for(int i=0;i<n-l+1;i++)
             {
-            int j=i+l-1;
-            if(l==2)
-            {
-                t[i][j]=(s[i]==s[j]);
-            }
-            else
-            {
-                t[i][j]=(s[i]==s[j])&&t[i+1][j-1];
+                int j=i+l-1;
+                if(l==2)
+                {
+                    t[i][j]=(s.charAt(i)==s.charAt(j));
+                }
+                else
+                {
+                    t[i][j]=(s.charAt(i)==s.charAt(j))&&t[i+1][j-1];
 
-            }
+                }
             }
         }
-        vector<int>dp(n);
+        int []dp=new int[n];
         for(int i=0;i<n;i++)
         {
-            if(t[0][i]==true){
-            dp[i]=0;
-            }
-            else
+            if(t[0][i]==true)
             {
-                dp[i]=INT_MAX;
+                dp[i]=0;
+            }
+            else{
+                dp[i]=Integer.MAX_VALUE;
                 for(int k=0;k<i;k++)
                 {
                     if(t[k+1][i]==true&&1+dp[k]<dp[i])
@@ -41,15 +43,11 @@ int solve(string &s)
                     }
                 }
             }
+
+
+            
         }
         return dp[n-1];
-
-}
-    int minCut(string s) { 
-        int n=s.length();
-        if(n==0||n==1)
-        return 0;
-         return solve(s);
         
     }
-};
+}
