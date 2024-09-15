393944c40d9ca7521dcda634c2f055ec41bbaa3d
Nithin Emmadishetti <nithin200133@gmail.com>
2024-09-15 16:54:45 +0530

    Modified longest_Substring_Conatins_Vowels_in_Even_counts.java Tc=O(N) Sc=O(1)

--- longest_Substring_Conatins_Vowels_in_Even_counts.java
+++ longest_Substring_Conatins_Vowels_in_Even_counts.java
@@ -9,7 +9,7 @@ class Solution {
         {
             if(s.charAt(i)=='a')
             {
-                a[0]=(a[0]^1);
+                a[0]=(a[0]^1);//(a[0]+1)%2
             }
             else if(s.charAt(i)=='e')
             {
@@ -38,6 +38,48 @@ class Solution {
             }
             else{
                 mp.put(currstate,i);
+            }
+        }
+        return result;
+        
+    }
+}
+//Approach Two with mask
+class Solution {
+    public int findTheLongestSubstring(String s) {
+        Map<Integer,Integer> mp=new HashMap<>();
+        int mask=0;
+        mp.put(mask,-1);
+        int result=0;
+        for(int i=0;i<s.length();i++)
+        {
+            if(s.charAt(i)=='a')
+            {
+                mask=mask^(1<<0);
+            }
+            else if(s.charAt(i)=='e')
+            {
+                  mask=mask^(1<<1);
+            }
+             else if(s.charAt(i)=='i')
+            {
+                mask=mask^(1<<2);
+            }
+              else if(s.charAt(i)=='o')
+            {
+                  mask=mask^(1<<3);
+            }
+              else if(s.charAt(i)=='u')
+            {
+                mask=mask^(1<<4);
+            }
+           
+            if(mp.containsKey(mask))
+            {
+                result=Math.max(result,i-mp.get(mask));
+            }
+            else{
+                mp.put(mask,i);
             }
         }
         return result;
