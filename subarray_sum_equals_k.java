c016ad2e5f24c84b9b7f00b26c9599b9d9205748
Nithin Emmadishetti <nithin200133@gmail.com>
2024-10-04 09:44:38 +0530

    subarray_sum_equals_k.java

--- subarray_sum_equals_k.java
+++ subarray_sum_equals_k.java
@@ -1,1 +1,21 @@
+class Solution {
+    public int subarraySum(int[] nums, int k) {
+Map<Integer,Integer> mp=new HashMap<>();
+int n=nums.length;
+mp.put(0,1);
+int result=0;
+int curr=0;
+for(int i=0;i<n;i++)
+{
+    curr+=nums[i];
+    if(mp.containsKey(curr-k))
+    {
+        result+=mp.get(curr-k);
+    }
+    mp.put(curr,mp.getOrDefault(curr, 0) + 1);
+
+}
+return result;
         
+    }
+}
