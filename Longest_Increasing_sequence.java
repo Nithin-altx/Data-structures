e75afb24b59b9681938b825a8ae171af6a9e1939
Nithin Emmadishetti <nithin200133@gmail.com>
2024-10-31 11:41:43 +0530

    Longest_Increasing_sequence.java Modified

--- Longest_Increasing_sequence.java
+++ Longest_Increasing_sequence.java
@@ -19,3 +19,39 @@ class Solution {
 
     }
 }
+//***************Rec+memo****************
+class Solution {
+    int n;
+    int t[][];
+    int solve(int i,int p,int []nums)
+    {
+       
+        if(i>=n)
+        return 0;
+        if(p!=-1&&t[i][p]!=-1)
+        {
+            return t[i][p];
+        }
+        int take=0;
+        if(p==-1||nums[p]<nums[i])
+        {
+             take=1+solve(i+1,i,nums);
+        }
+        int skip=solve(i+1,p,nums);
+        if(p!=-1)
+        t[i][p]=Math.max(take,skip);
+
+        return  Math.max(take,skip);
+        
+    }
+    public int lengthOfLIS(int[] nums) {
+        n=nums.length;
+        t=new int[n+1][n+1];
+        for(int i=0;i<n;i++)
+        {
+            Arrays.fill(t[i],-1);
+        }
+        return solve(0,-1,nums);
+        
+    }
+}
