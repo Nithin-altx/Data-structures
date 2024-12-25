87d19c86bef87938bdcff35b2247551da3ece2f0
Nithin Emmadishetti <nithin200133@gmail.com>
2024-12-25 19:18:14 +0530

    Find_Largest_value_in_Each_Tree_Row.java

--- Find_Largest_value_in_Each_Tree_Row.java
+++ Find_Largest_value_in_Each_Tree_Row.java
@@ -31,3 +31,42 @@ class Solution {
         return res;
     }
 }
+//DFS TC=O(N) SC=O(1)//STACK SPACE O(N)
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+     List<Integer> res=new ArrayList<>();
+    void dfs(TreeNode root,int depth)
+    {
+        if(root==null)
+        {
+            return;
+        }
+        if(depth==res.size())
+        {
+            res.add(root.val);
+        }
+        else{
+            res.set(depth,Math.max(root.val,res.get(depth)));
+        }
+        dfs(root.left,depth+1);
+        dfs(root.right,depth+1);
+    }
+    public List<Integer> largestValues(TreeNode root) {
+        dfs(root,0);
+        return res;
+        
+    }
+}
+
