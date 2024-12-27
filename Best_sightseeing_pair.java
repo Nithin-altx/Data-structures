7ebd06b8a1bb096ebf553fa8deb9705728f61c09
Nithin Emmadishetti <nithin200133@gmail.com>
2024-12-27 08:36:17 +0530

    Best_sightseeing_pair.java

--- Best_sightseeing_pair.java
+++ Best_sightseeing_pair.java
@@ -1,6 +1,7 @@
 class Solution {
     public int maxScoreSightseeingPair(int[] values) {
         //Tc=O(N)
+//using no space
         int n=values.length;
         int maxleft=values[0]+0;
         int maxscore=Integer.MIN_VALUE;
@@ -9,6 +10,24 @@ class Solution {
             maxscore=Math.max(maxscore,maxleft+values[j]-j);
             maxleft=Math.max(maxleft,values[j]+j);
 
+        }
+        return maxscore;
+    }
+}
+//Use space
+class Solution {
+    public int maxScoreSightseeingPair(int[] values) {
+        int v=values.length;
+        int maxleft[]=new int[v];
+        maxleft[0]=values[0];
+        for(int i=1;i<v;i++)
+        {
+            maxleft[i]=Math.max(values[i]+i,maxleft[i-1]);
+        }
+        int maxscore=Integer.MIN_VALUE;
+        for(int j=1;j<v;j++)
+        {
+            maxscore=Math.max(maxscore,maxleft[j-1]+values[j]-j);
         }
         return maxscore;
     }
