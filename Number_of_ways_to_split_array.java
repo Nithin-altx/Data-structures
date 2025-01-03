a18b9c67c663d899cd3701c4c6ea6a74ea5cf799
Nithin Emmadishetti <nithin200133@gmail.com>
2025-01-03 11:09:34 +0530

    Number_of_ways_to_split_array.java

--- Number_of_ways_to_split_array.java
+++ Number_of_ways_to_split_array.java
@@ -45,3 +45,32 @@ class Solution {
         return count;
     }
 }
+//Approach3
+class Solution {
+    public int waysToSplitArray(int[] nums) {
+        int n=nums.length;
+        long leftsum[]=new long[n-1];
+        long rightsum[]=new long[n];
+        long sum=0;
+        for(int i=0;i<n-1;i++)
+        {
+            sum+=nums[i];
+            leftsum[i]=sum;
+        }
+        sum=0;
+        for(int j=n-1;j>0;j--)
+        {
+            sum+=nums[j];
+            rightsum[j]=sum;
+        }
+        int count=0;
+        for(int k=0;k<n-1;k++)
+        {
+            if(leftsum[k]>=rightsum[k+1])
+            {
+                count++;
+            }
+        }
+        return count;
+    }
+}
