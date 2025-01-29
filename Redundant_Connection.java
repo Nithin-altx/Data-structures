5e095bbda2fe19d323cd44cc078a811a57c9c2c4
Nithin Emmadishetti <nithin200133@gmail.com>
2025-01-29 21:11:20 +0530

    Redundant_Connection.java

--- Redundant_Connection.java
+++ Redundant_Connection.java
@@ -33,3 +33,44 @@ class Solution {
         
     }
 }
+//Bfs
+class Solution {
+    boolean Bfs(Map<Integer,List<Integer>> adj,int u,int v,boolean[]vis)
+    {
+        Queue<Integer> pq=new LinkedList<>();
+        pq.offer(u);
+        while(!pq.isEmpty())
+        {
+            int edg=pq.poll();
+             vis[edg]=true;
+            if(edg==v)
+            return true;
+        for(int ngbr:adj.getOrDefault(edg,new ArrayList<>()))
+        {
+            if(vis[ngbr])
+            continue;
+            pq.offer(ngbr);
+        }
+        }
+        return false;
+
+    }
+    public int[] findRedundantConnection(int[][] edges) {
+        int n=edges.length;
+        Map<Integer,List<Integer>> adj=new HashMap<>();
+        for(int i=0;i<n;i++)
+        {
+            int u=edges[i][0];
+            int v=edges[i][1];
+            boolean vis[]=new boolean[n+1];
+            if(adj.containsKey(u)&&adj.containsKey(v)&&Bfs(adj,u,v,vis))
+            {
+                return edges[i];
+            }
+            adj.computeIfAbsent(u,k->new ArrayList<>()).add(v);
+            adj.computeIfAbsent(v,k->new ArrayList<>()).add(u);
+        }
+        return new int[]{};
+        
+    }
+}
