be6b76c9b70dfd2e6e74e3bb1991f233cb781b2c
Nithin Emmadishetti <nithin200133@gmail.com>
2025-02-04 22:19:04 +0530

    Diameter_of_Binary_Tree.java

---
+++ Diameter_of_Binary_Tree.java
@@ -0,0 +1,28 @@
+//Tc=O(N)
+//Sc=O(1)
+class Solution {
+    int maxd=0;
+    int solve(Node root)
+    {
+        if(root==null)
+        return 0;
+        int left=0;
+        if(root.left!=null)
+        {
+            left=1+solve(root.left);
+        }
+        int right=0;
+        if(root.right!=null)
+        {
+            right=1+solve(root.right);
+        }
+        maxd=Math.max(maxd,left+right);
+        return Math.max(left,right);
+    }
+    int diameter(Node root) {
+        // Your code here
+        int d=solve(root);
+        return maxd;
+        
+    }
+}
