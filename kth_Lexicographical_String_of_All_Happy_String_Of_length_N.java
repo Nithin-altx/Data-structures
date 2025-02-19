94d68e3c694d98dac9ff0279f5374da66100a6ae
Nithin Emmadishetti <nithin200133@gmail.com>
2025-02-19 18:08:17 +0530

    kth_Lexicographical_String_of_All_Happy_String_Of_length_N.java

---
+++ kth_Lexicographical_String_of_All_Happy_String_Of_length_N.java
@@ -0,0 +1,29 @@
+class Solution {
+    void solve(int n, List<String> ls,StringBuilder res)
+    {
+        if(res.length()==n)
+        {
+            ls.add(res.toString());
+            return;
+        }
+        for(char ch='a';ch<='c';ch++)
+        {
+            if(res.isEmpty()||res.charAt(res.length()-1)!=ch)
+            {
+                res.append(ch);
+                solve(n,ls,res);
+                res.deleteCharAt(res.length()-1);
+            }
+        }
+
+    }
+    public String getHappyString(int n, int k) {
+        List<String> ls=new ArrayList<>();
+        StringBuilder res=new StringBuilder();
+        solve(n,ls,res);
+        if(k>ls.size())
+        return "";
+        return ls.get(k-1);
+        
+    }
+}
