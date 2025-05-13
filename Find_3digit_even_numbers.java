f722dbad99a3cd73636fe321d661217a5dd2f049
Nithin Emmadishetti <nithin200133@gmail.com>
2025-05-13 08:46:24 +0530

    Find_3digit_even_numbers.java

--- Find_3digit_even_numbers.java
+++ Find_3digit_even_numbers.java
@@ -50,3 +50,49 @@ class Solution {
         return r;
     }
 }
+//Optimal
+class Solution {
+    public int[] findEvenNumbers(int[] digits) {
+        //Tc=O(1)
+        //sc=O(1)
+       int mp[]=new int[10];
+        for(int x:digits)
+        {
+            mp[x]++;
+        }
+        List<Integer> ls=new ArrayList<>();
+        for(int i=1;i<=9;i++)
+        {
+            if(mp[i]==0)
+            {
+                continue;
+            }
+             mp[i]--;
+            for(int j=0;j<=9;j++)
+            {
+                if(mp[j]==0)
+                continue;
+
+                mp[j]--;
+                for(int k=0;k<=8;k+=2)
+                {
+                    if(mp[k]==0)
+                    continue;
+
+                    mp[k]--;
+                    int num=(i*100)+(j*10)+k;
+                    ls.add(num);
+                    mp[k]++;
+                }
+                mp[j]++;
+            }
+            mp[i]++;
+        }
+        int res[]=new int[ls.size()];
+        for(int i=0;i<ls.size();i++)
+        {
+            res[i]=ls.get(i);
+        }
+        return res;
+    }
+}
